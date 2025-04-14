d3c4c1d57ca5177606aadadd2f921cad76f03326
sevaphasol <sevsol2512@gmail.com>
2025-04-14 21:24:09 +0300

    added python script

--- src/testing.cpp
+++ src/testing.cpp
@@ -7,16 +7,14 @@
 
 //——————————————————————————————————————————————————————————————————————————————
 
-#define GREEN "\033[1;32m"
-#define RED   "\033[1;31m"
-#define WHITE "\033[0m"
+const char* const TestData      = "test.bin";
+const size_t      HashTableSize = 100000;
 
 //——————————————————————————————————————————————————————————————————————————————
 
 struct test_ctx_t
 {
     char*       buffer;
-    size_t      buffer_size;
     data_key_t* keys;
     data_t*     data;
     size_t      n_strings;
@@ -24,50 +22,53 @@ struct test_ctx_t
 
 //——————————————————————————————————————————————————————————————————————————————
 
-uint64_t no_hash(data_key_t key);
-static uint64_t sdbm_hash            (data_key_t key);
-static uint64_t get_file_size        (FILE* const file_ptr);
-static void test_ctx_ctor            (test_ctx_t* test_ctx);
-static void test_ctx_dtor            (test_ctx_t* test_ctx);
-static int  fill_hash_table          (hash_table_t* hash_table,
-                                      test_ctx_t* test_ctx);
-static int  find_elems_in_hash_table (hash_table_t* hash_table,
-                                      test_ctx_t* test_ctx);
-static void get_keys_and_data        (test_ctx_t* test_ctx);
-static void read_test_file           (test_ctx_t* test_ctx);
-static void read_test_file           (test_ctx_t* test_ctx);
-static void get_n_strings            (test_ctx_t* test_ctx);
+enum test_status_t
+{
+    TEST_SUCCESS          = 0,
+    TEST_OPEN_FILE_ERROR  = 1,
+    TEST_READ_SIZES_ERROR = 2,
+    TEST_STD_ALLOC_ERROR  = 3,
+    TEST_READ_DATA_ERROR  = 4,
+    TEST_ADDING_FAILURE   = 5,
+    TEST_FINDING_FAILURE  = 6,
+};
+
+//——————————————————————————————————————————————————————————————————————————————
+
+uint64_t sdbm_hash(data_key_t key);
+
+test_status_t test_ctx_dtor (test_ctx_t* ctx);
+test_status_t get_test_data (test_ctx_t* ctx);
+test_status_t test_ctx_dtor (test_ctx_t* ctx);
+test_status_t test_adding   (test_ctx_t* ctx, hash_table_t* hash_table);
+test_status_t test_finding  (test_ctx_t* ctx, hash_table_t* hash_table);
 
 //——————————————————————————————————————————————————————————————————————————————
 
 int main()
 {
-    test_ctx_t test_ctx = {};
-    test_ctx_ctor(&test_ctx);
+    test_ctx_t ctx = {};
+    get_test_data(&ctx);
 
     //--------------------------------------------------------------------------
 
     hash_table_t hash_table = {};
-    hash_table_ctor(&hash_table, 1000000, sdbm_hash);
+    hash_table_ctor(&hash_table, HashTableSize, sdbm_hash);
 
     //--------------------------------------------------------------------------
 
-    if (fill_hash_table(&hash_table, &test_ctx)) {
-        printf(RED "Error while adding to hash table\n" WHITE);
-    } else {
-        printf(GREEN "Added test info to hash table successfully\n" WHITE);
+    if (test_adding(&ctx, &hash_table)) {
+        fprintf(stderr, "test_adding failure\n");
     }
 
-    if (find_elems_in_hash_table(&hash_table, &test_ctx)) {
-        printf(RED "Error while finding in hash table\n" WHITE);
-    } else {
-        printf(GREEN "Finded test info in hash table successfully\n" WHITE);
+    if (test_finding(&ctx, &hash_table)) {
+        fprintf(stderr, "test_finding failure\n");
     }
 
     //--------------------------------------------------------------------------
 
     hash_table_dtor(&hash_table);
-    test_ctx_dtor(&test_ctx);
+    test_ctx_dtor(&ctx);
 
     return 0;
 }
@@ -88,156 +89,112 @@ uint64_t sdbm_hash(data_key_t key)
 
 //==============================================================================
 
-void test_ctx_ctor(test_ctx_t* test_ctx)
+test_status_t test_adding(test_ctx_t* ctx, hash_table_t* hash_table)
 {
-    read_test_file(test_ctx);
-    get_keys_and_data(test_ctx);
-}
-
-//==============================================================================
-
-void test_ctx_dtor(test_ctx_t* test_ctx)
-{
-    free(test_ctx->keys);
-    free(test_ctx->buffer);
-}
-
-//==============================================================================
-
-int fill_hash_table(hash_table_t* hash_table, test_ctx_t* test_ctx)
-{
-    size_t n_strings  = test_ctx->n_strings;
-    data_key_t* keys  = test_ctx->keys;
-    data_t*     data  = test_ctx->data;
+    size_t      n_strings  = ctx->n_strings;
+    data_key_t* keys       = ctx->keys;
+    data_t*     data       = ctx->data;
 
     //--------------------------------------------------------------------------
 
     for (int i = 0; i < n_strings; i++) {
         if (hash_table_add(hash_table, keys[i], data[i])) {
-            return -1;
+            return TEST_ADDING_FAILURE;
         }
     }
 
-    return 0;
+    //--------------------------------------------------------------------------
+
+    return TEST_SUCCESS;
 }
 
 //==============================================================================
 
-int find_elems_in_hash_table(hash_table_t* hash_table, test_ctx_t* test_ctx)
+test_status_t test_finding(test_ctx_t* ctx, hash_table_t* hash_table)
 {
-    size_t      n_strings = test_ctx->n_strings;
-    data_key_t* keys      = test_ctx->keys;
-    data_t*     data      = test_ctx->data;
-    data_t      result;
+    size_t      n_strings = ctx->n_strings;
+    data_key_t* keys      = ctx->keys;
+    data_t*     data      = ctx->data;
+    data_t      result    = 0;
+
+    //--------------------------------------------------------------------------
 
     for (int i = 0; i < n_strings; i++) {
         hash_table_find(hash_table, keys[i], &result);
 
         if (result != data[i]) {
-            return -1;
-        }
-    }
-
-    return 0;
-}
-
-//==============================================================================
-
-void get_keys_and_data(test_ctx_t* test_ctx)
-{
-    get_n_strings(test_ctx);
-    size_t n_strings  = test_ctx->n_strings;
-
-    //--------------------------------------------------------------------------
-
-    data_key_t* keys  = (data_key_t*) calloc(n_strings * sizeof(data_key_t) +
-                                             n_strings * sizeof(data_t),
-                                             sizeof(char));
-
-    //--------------------------------------------------------------------------
-
-    data_t* data  = (data_t*) (keys + n_strings);
-
-    //--------------------------------------------------------------------------
-
-    keys[0] = (data_key_t) strtok(test_ctx->buffer, " \n");
-    data[0] = (data_t)     atoi(strtok(NULL,        " \n"));
-
-    for (int i = 1; i < n_strings; i++) {
-        keys[i] = (data_key_t)      strtok(NULL, " \n");
-        data[i] = (data_t)     atoi(strtok(NULL, " \n"));
-    }
-
-    //--------------------------------------------------------------------------
-
-    test_ctx->keys = keys;
-    test_ctx->data = data;
-}
-
-//==============================================================================
-
-void read_test_file(test_ctx_t* test_ctx)
-{
-    FILE* file_ptr = fopen("test_data.bin", "rb");
-    VERIFY(!file_ptr, return);
-
-    //--------------------------------------------------------------------------
-
-    size_t buffer_size = 0;
-    char*  buffer      = nullptr;
-
-    buffer_size = get_file_size(file_ptr);
-
-    buffer = (char*) calloc(buffer_size, sizeof(char));
-    VERIFY(fread(buffer, sizeof(char), buffer_size, file_ptr) != buffer_size,
-           return)
-
-    //--------------------------------------------------------------------------
-
-    fclose(file_ptr);
-
-    //--------------------------------------------------------------------------
-
-    test_ctx->buffer      = buffer;
-    test_ctx->buffer_size = buffer_size;
-}
-
-//==============================================================================
-
-void get_n_strings(test_ctx_t* test_ctx)
-{
-    size_t n_strings = 0;
-    size_t size      = test_ctx->buffer_size;
-    char*  buffer    = test_ctx->buffer;
-
-    //--------------------------------------------------------------------------
-
-    for (int i = 0; i < size; i++) {
-        if (buffer[i] == '\n') {
-            n_strings++;
+            return TEST_FINDING_FAILURE;
         }
     }
 
     //--------------------------------------------------------------------------
 
-    test_ctx->n_strings = n_strings;
+    return TEST_SUCCESS;
 }
 
 //==============================================================================
 
-uint64_t get_file_size(FILE* const file_ptr)
+test_status_t get_test_data(test_ctx_t* ctx)
 {
-    struct stat file_status = {};
+    FILE* test_file = fopen(TestData, "rb");
+    VERIFY(!test_file, return TEST_OPEN_FILE_ERROR);
 
     //--------------------------------------------------------------------------
+    // Reading sizes through struct, so we can use only one fread().
 
-    if (fstat(fileno(file_ptr), &file_status) < 0) {
-        return -1;
+    struct {
+        size_t file_size;
+        size_t keys_size;
+        size_t n_strings;
+    } sizes = {};
+
+    VERIFY(fread(&sizes, sizeof(sizes), 1, test_file) != 1,
+           return TEST_OPEN_FILE_ERROR);
+
+    //--------------------------------------------------------------------------
+    // Reading whole file (except sizes) in ctx->buffer.
+
+    ctx->buffer = (char*) calloc(sizes.file_size +
+                                 sizes.n_strings * sizeof(data_key_t),
+                                 sizeof(char));
+    VERIFY(!ctx->buffer, return TEST_STD_ALLOC_ERROR);
+
+    size_t remained_size = sizes.file_size - sizeof(sizes);
+
+    VERIFY(fread(ctx->buffer, sizeof(char), remained_size, test_file) != remained_size,
+           return TEST_READ_DATA_ERROR);
+
+    fclose(test_file);
+
+    //--------------------------------------------------------------------------
+    // Relative pointers are accumulated lens of strings (see script.py)
+    // We are making them absolute pointers to strings (keys)
+
+    ctx->data                 = (data_t*) (ctx->buffer + sizes.keys_size);
+    char** relative_pointers  = (char**)  (ctx->buffer + sizes.keys_size +
+                                           sizes.n_strings * sizeof(data_t));
+
+    char* base_pointer = ctx->buffer;
+
+    for (int i = 0; i < sizes.n_strings; i++) {
+        relative_pointers[i] += (size_t) base_pointer;
     }
 
+    ctx->keys      = relative_pointers;
+    ctx->n_strings = sizes.n_strings;
+
     //--------------------------------------------------------------------------
 
-    return file_status.st_size;
+    return TEST_SUCCESS;
 }
 
-//——————————————————————————————————————————————————————————————————————————————
+//==============================================================================
+
+test_status_t test_ctx_dtor(test_ctx_t* ctx)
+{
+    free(ctx->buffer);
+
+    return TEST_SUCCESS;
+}
+
+//==============================================================================
